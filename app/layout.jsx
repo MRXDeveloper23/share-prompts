16ce254fbbb508e7e55e6710cd7736f860805efd
Khurmatillo <khurmatillomamasoliev@gmail.com>
2023-05-30 17:40:34 +0500

    creating components and navigation ui completed

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import Nav from "@components/Nav";
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
