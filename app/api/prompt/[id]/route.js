57f00d361ee54357465382ea87094f5b109c25ea
Khurmatillo <khurmatillomamasoliev@gmail.com>
2023-06-05 17:50:38 +0500

    handling get patch and delete a prompt by id

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -2,26 +2,44 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-export const GET = async (req, res) => {
+export const GET = async (req, { params }) => {
   try {
     await connectToDB();
-    const prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), {
+    const prompt = await Prompt.findById(params.id).populate("creator");
+    if (!prompt) return new Response("Prompt not found", { status: 404 });
+    return new Response(JSON.stringify(prompt), {
       status: 200,
     });
   } catch (error) {
-    return new Response("Failed to fetch all prompts", {
+    return new Response("Failed to fetch a prompt", {
       status: 500,
     });
   }
 };
 
 //PATCH
-export const PATCH = async (req, res) => {
+export const PATCH = async (req, { params }) => {
+  const { prompt, tag } = await req.json();
   try {
     await connectToDB();
-    const newPrompt = null;
-  } catch (error) {}
-  return;
+    const existingPrompt = await Prompt.findById(params.id);
+    if (!existingPrompt)
+      return new Response("Prompt not found", { status: 404 });
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+    await existingPrompt.save();
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to update prompt!", { status: 500 });
+  }
 };
 //DELETE
+export const DELETE = async (req, { params }) => {
+  try {
+    await connectToDB();
+    await Prompt.findByIdAndRemove(params.id);
+    return new Response("Prompt deleted successfully!", { status: 200 });
+  } catch (error) {
+    return new Response("Failed to delete prompt!", { status: 500 });
+  }
+};
